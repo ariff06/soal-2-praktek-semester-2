aa37b30f60b6f1aa48b6a6e6ed5bf6ac53fec566
ahmadjumadi <ahmadjumadi@smkn1karawang.sch.id>
2023-03-13 10:13:10 +0700

    revisi soal

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-list_cash_flow = [
-  2500000, 5000000, -1000000, -2500000, 5000000, 10000000,
-  -5000000, 7500000, 10000000, -1500000, 25000000, -2500000
-  ]
-  total_pengeluaran = ...
-  total_pemasukan   = ...
-  for (let x in ... )
-  {
-    if (x > 0)
-    {
-      // Jumlahkan bilangan positif (total pemasukan)
-      .. += list_cash_flow[...]
-    }
-    else
-    {
-      // jumlahkan bilangan negarif (total pengeluaran)
-      .. += ...[x]
-    }
-total_pengeluaran *= -1
-  }
-  console.log(`Total Pengeluaran adalah : ${...}`) 
-  console.log(`Total Pemasukan adalah : ${...}`)
-
-  list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
+list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
 
 kendaraan_genap = ...
 kendaraan_ganjil = 0
