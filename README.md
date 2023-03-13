1f712d2b6e684c4c2cd1ae72b1611cc3a56c2fd4
ahmadjumadi <ahmadjumadi@smkn1karawang.sch.id>
2023-03-13 10:11:37 +0700

    soal 2 pplg

---
+++ README.md
@@ -0,0 +1,12 @@
+## TUGAS PRAKTEK
+
+Dalam program kali ini, aku diminta untuk menghitung jumlah kendaraan yang memiliki plat nomor ganjil dan genap yang dimiliki sebuah perushaan.
+
+## Tugas : 
+Kalau begitu aku harus membedakan sekaligus menjumlahkan kendaraan yang memiliki ``plat nomor genap`` dan ``plat nomor ganjil``. setelah itu aku harus mengetahui total dari kendaraan yang memiliki ``plat nomor genap`` dan ``plat nomor ganjil``.
+
+
+## Referensi
+- JavaScript For Loop [disini](https://www.w3schools.com/js/js_loop_forin.asp)
+- JavaScript For In [disini](https://www.w3schools.com/js/js_loop_forin.asp)
+- JavaScript if, else, and else if [disini](https://www.w3schools.com/js/js_if_else.asp)
---
+++ index.js
@@ -0,0 +1,46 @@
+list_cash_flow = [
+  2500000, 5000000, -1000000, -2500000, 5000000, 10000000,
+  -5000000, 7500000, 10000000, -1500000, 25000000, -2500000
+  ]
+  total_pengeluaran = ...
+  total_pemasukan   = ...
+  for (let x in ... )
+  {
+    if (x > 0)
+    {
+      // Jumlahkan bilangan positif (total pemasukan)
+      .. += list_cash_flow[...]
+    }
+    else
+    {
+      // jumlahkan bilangan negarif (total pengeluaran)
+      .. += ...[x]
+    }
+total_pengeluaran *= -1
+  }
+  console.log(`Total Pengeluaran adalah : ${...}`) 
+  console.log(`Total Pemasukan adalah : ${...}`)
+
+  list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
+
+kendaraan_genap = ...
+kendaraan_ganjil = 0
+
+for (let x in ...)
+{
+  if (list_plat_nomor[...] % 2 == 0)
+  {
+    // Jumlahkan kendaraan yang memiliki plat nomor genap
+    ... += 1
+  } 
+  else 
+  {
+    // Jumlahkan kendaraan yang memiliki plat nomor ganjil
+    ... ... ...
+  }
+}
+total_kendaraan = ... + ...
+
+console.log(`Jumlah mobil dengan plat nomor genap adalah : ${...}`) 
+console.log(`Jumlah mobil dengan plat nomor ganjil adalah : ${...}`)
+console.log(`Total seluruh kendaraan adalah : ${..}`)
